a1ceb39981365a03b2b70dc54e75461452916c8d
Vashu8 <vashujainvps11@gmail.com>
2020-01-08 11:34:48 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>LOGIN PAGE </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-8">
+            <form action="" method="post">
+            <label for="">Username</label>
+            <input type="text" class="form-control" name="uname">
+            <label for="">Password</label>
+            <input type="password" class="form-control" name="pass">
+            <input type="submit" class="btn btn-success" name="sub"><br>
+            <a href="reg.php" class="btn btn-danger">Register Yourself</a>
+            </form>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ reg.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+</head>
+<body>
+        <div class="container">
+        <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+        <form action="" method="post">
+        <label for="">Name</label>
+        <input type="text" class="form-control" name="name" required>
+        <label for="">Email</label>
+        <input type="text" class="form-control" name="email">
+        <label for="">Password</label>
+        <input type="password" class="form-control" name="pass">
+        <label for="">Confirm Password</label>
+        <input type="password" class="form-control" name="cpass">
+        <input type="submit" class="btn btn-success" name="sub">
+        </form>
+        <a href="index.php" class="btn btn-primary">Back Login Page</a>
+        </div>
+        <div class="col-md-2"></div>
+        </div>
+        </div> 
+</body>
+</html>
